82f0319ddacc5bbd63284eaba4f854c1e9cc8a9b
k <daanrod93@gmail.com>
2020-07-30 17:26:51 -0300

    Dice simulator finished.

--- dice_simulator.py
+++ dice_simulator.py
@@ -1,3 +1,31 @@
-# Simulate a dice
-# Simulate a dice roll 1 a 6 with a
-# Ask if roll or not
+import random
+import PySimpleGUI
+
+
+class DiceSimulator:
+    # Values of the dice and message for users
+    def __init__(self):
+        self.min_value = 1
+        self.max_value = 6
+        self.message = 'Você gostaria de rolar o dado? (s / n)'
+
+    # Receive response from users
+    def start(self):
+        response = input(self.message)
+        try:
+            if response == 's':
+                self.dicevalue()
+            elif response == 'n':
+                print('Agradecemos sua particição!')
+            else:
+                print('Favor digitar "s" para sim e "n" para não!')
+        except:
+            print('Ocorreu um erro no programa.')
+
+    # Show roll result
+    def dicevalue(self):
+        print(random.randint(self.min_value, self.max_value))
+
+
+simulator = DiceSimulator()
+simulator.start()
