fc1475a9802b1e5f7e44012dc26917ec4880eb7b
k <daanrod93@gmail.com>
2020-07-30 21:48:23 -0300

    Dice with simple GUI finished.

--- dice_simulator.py
+++ dice_simulator.py
@@ -1,5 +1,5 @@
 import random
-import PySimpleGUI
+import PySimpleGUI as sg
 
 
 class DiceSimulator:
@@ -7,20 +7,25 @@ class DiceSimulator:
     def __init__(self):
         self.min_value = 1
         self.max_value = 6
-        self.message = 'Você gostaria de rolar o dado? (s / n)'
+
+        # Layout
+        self.layout = [
+            [sg.Text('Rolar o dado?')],
+            [sg.Button('Sim'), sg.Button('Não')]
+        ]
+        # Create a window
+        self.window = sg.Window('Simulador de dado', self.layout)
+
+        # Read values
+        self.events, self.values = self.window.Read()
 
     # Receive response from users
     def start(self):
-        response = input(self.message)
-        try:
-            if response == 's':
-                self.dicevalue()
-            elif response == 'n':
-                print('Agradecemos sua particição!')
-            else:
-                print('Favor digitar "s" para sim e "n" para não!')
-        except:
-            print('Ocorreu um erro no programa.')
+        # Do something with this values
+        if self.events == 'Sim':
+            self.dicevalue()
+        elif self.events == 'Não':
+            print('Agradecemos sua particição!')
 
     # Show roll result
     def dicevalue(self):
