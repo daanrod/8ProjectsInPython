fe88a072dbd72a1296cc53907b0b1221e560c4bb
k <daanrod93@gmail.com>
2020-07-30 23:06:52 -0300

    Added some information to the code.

--- hit_the_number.py
+++ hit_the_number.py
@@ -13,21 +13,26 @@ class HitTheNumber:
         self.AskRandomValue()
         try:
             while self.try_again:
+                # Informs if the given value is higher than correct value
                 if int(self.given_value) > self.random_value:
                     print('Tente um valor menor!')
                     self.AskRandomValue()
+                # Informs if the given value is lower than the correct value
                 elif int(self.given_value) < self.random_value:
                     print('Tente um valor maior!')
                     self.AskRandomValue()
+                # Informs thats the correctly value
                 if int(self.given_value) == self.random_value:
                     self.try_again = False
                     print('ACERTOU MIZERAVI !!')
         except:
             print('Apenas numero!')
 
+    # Ask a value
     def AskRandomValue(self):
         self.given_value = input('Acerto o numero de 1 a 100: ')
 
+    # Generates a random value
     def GenRandomNumber(self):
         self.random_value = random.randint(self.min_value, self.max_value)
 
